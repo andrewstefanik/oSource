e7a08580d3945da9e59f0d9ce8d040f24b15a4fa
Michael Miller <mjmill22@asu.edu>
2016-11-30 17:17:44 -0700

    changes to the header

--- public/js/ngApp/app/HeaderController.js
+++ public/js/ngApp/app/HeaderController.js
@@ -1,5 +1,17 @@
-angular.module('oSource').controller('NavbarController', ['$scope', '$auth', function($scope, $auth) {
+angular.module('oSource').controller('NavbarController', ['$scope', '$auth', 'ProfileService', function($scope, $auth, ProfileService) {
     $scope.isAuthenticated = function() {
         return $auth.isAuthenticated();
     };
+
+    $scope.getProfile = function() {
+      ProfileService.getProfile()
+        .then(function(response) {
+          $scope.user = response.data;
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+
+    $scope.getProfile()
 }]);
