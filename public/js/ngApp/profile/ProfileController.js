8c84d1d065104b66702b39c498b8c404d8e945e2
Michael Miller <mjmill22@asu.edu>
2016-12-08 13:03:19 -0700

    displaying user's oSource repos on their homepage

--- public/js/ngApp/profile/ProfileController.js
+++ public/js/ngApp/profile/ProfileController.js
@@ -3,13 +3,18 @@ angular.module('oSource')
     $scope.title = 'Repos';
 
     ProfileService.getProfile().then(function (response) {
-        userName = response.data.userName;
+        var userName = response.data.userName;
         $scope.userName = userName;
-        SearchService.user.get(
-            {user: userName}, function (res) {
-                var data = JSON.parse (res.data);
-                $scope.repoList = data;
-                console.log (data);
-            });
-        });
+        SearchService.user.get({user: userName}, function (res) {
+            var data = JSON.parse (res.data);
+            $scope.repoList = data;
+            console.log (data);
+        })
+        ProfileService.getRepos($scope.userName).then(function(res) {
+            console.log('Yes sir', res.data);
+            // var repoData = JSON.parse(res.data);
+            $scope.repos = res.data;
+            // console.log(repoData);
+        })
+    })
 }]);
